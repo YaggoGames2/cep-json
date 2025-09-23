fa86c250e670b82c10d109a9d9c79ec977183595
YAGGO ROSA DE ALMEIDA SILVA <rosayaggo2@gmail.com>
2025-09-23 17:22:23 -0300

    exibir a mensagem do código

--- pages/cep.php
+++ pages/cep.php
@@ -24,14 +24,49 @@ if (isset($cep) == false || strlen($cep) != 8) {
 
         if (isset($dados["erro"]) == true) {
             $mensagem = "CEP não encontrado.";
-        } else {
-            echo "<h2> Endereço encontrado </h2>";
-            echo "<input type='text' value='{$dados['logradouro']}' disabled> <br>";
-            echo "<input type='text' value='{$dados['complemento']}' disabled> <br>";
-            echo "<input type='text' value='{$dados['bairro']}' disabled> <br>";
-            echo "<input type='text' value='{$dados['localidade']}' disabled> <br>";
-            echo "<input type='text' value='{$dados['estado']}' disabled> <br>";
-        }
+        } //else {
+           // echo "<h2> Endereço encontrado </h2>";
+           // echo "<input type='text' value='{$dados['logradouro']}' disabled> <br>";
+           // echo "<input type='text' value='{$dados['complemento']}' disabled> <br>";
+           // echo "<input type='text' value='{$dados['bairro']}' disabled> <br>";
+           // echo "<input type='text' value='{$dados['localidade']}' disabled> <br>";
+           // echo "<input type='text' value='{$dados['estado']}' disabled> <br>";
+       // }
     }
-    echo "<p>{$mensagem}</p>";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="cep-buscado">
+    <span id="error"><? $mensagem ?></span>
+        <div>
+            <label>logradouro: </label>
+            <input type="text" value="<?= isset($dados['logradouro']) ? $dados['logradouro'] : 'CEP não encontrado.' ?>"disabled>
+        </div>
+        <div>
+            <label>Complemento: </label>
+            <input type="text" value="<?= isset($dados['complemento']) ? $dados['complemento'] : 'CEP não encontrado.' ?>"disabled>
+        </div>
+        <div>
+            <label>Bairro: </label>
+            <input type="text" value="<?= isset($dados['bairro']) ? $dados['bairro'] : 'CEP não encontrado.' ?>"disabled>
+        </div>
+        <div>
+            <label>localidade: </label>
+            <input type="text" value="<?= isset($dados['localidade']) ? $dados['localidade'] : 'CEP não encontrado.' ?>"disabled>
+        </div>
+        <div>
+            <label>Estado: </label>
+            <input type="text" value="<?= isset($dados['uf']) ? $dados['uf'] : 'CEP não encontrado.'?>"disabled>
+        </div>
+</div>
+</body>
+</html>
